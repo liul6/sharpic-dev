6c7e3ba082b961ae76154410f76b92c1a06656dd
liul6 <yiqiu.liu@gmail.com>
2016-12-02 21:45:22 -0500

    'testing'

--- src/main/java/com/springjava4dev/web/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springjava4dev.web;
-
-import com.sharpic.domain.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-
-    public User findByUsername(String username);
-}
--- src/test/java/com/springjava4dev/web/WebsecurityTutorialApplicationTests.java
+++ src/test/java/com/springjava4dev/web/WebsecurityTutorialApplicationTests.java
@@ -1,5 +1,6 @@
 package com.springjava4dev.web;
 
+import com.sharpic.WebsecurityTutorialApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
