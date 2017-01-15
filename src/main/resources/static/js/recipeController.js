1daf1e724122681230fe682d4bb1b48f9041180e
liul6 <yiqiu.liu@gmail.com>
2017-01-15 09:51:12 -0500

    Implemented client product page using ui-grid

--- src/main/resources/static/js/recipeController.js
+++ src/main/resources/static/js/recipeController.js
@@ -1,4 +1,4 @@
-sharpicApp.controller('recipeController', function($rootScope, $http, $location, $route, $scope, DTOptionsBuilder) {
+sharpicApp.controller('recipeController', function($rootScope, $http, $location, $route, $scope, $modal) {
     $scope.clientName = null;
     $scope.clientNames = [];
 
@@ -20,13 +20,15 @@ sharpicApp.controller('recipeController', function($rootScope, $http, $location,
 
     $scope.populateDefault = function(selectedClientName) {
         $scope.clientName = selectedClientName;
+        var descriptionCelltemplate = '<div ng-click="grid.appScope.modifyRecipe(row.entity)"><a href="">{{row.entity.description}}</a></div>';
+        // '<div><a href ng-click="modifyRecipe()">{{row.entity.description}}</a></div>'
 
         $scope.recipesOptions = {
             data: [],
             enableColumnMenus: false,
             columnDefs: [
                 {name: 'recipeName', displayName: 'Recipe Name', width : '30%', enableCellEdit : false  },
-                {name: 'description', displayName: 'Recipe Items', enableCellEdit : false },
+                {name: 'description', displayName: 'Recipe Items', enableCellEdit : false, cellTemplate : descriptionCelltemplate },
                 {name: 'action', displayName: '', width : '3%', cellTemplate: '<button class="btn btn-danger btn-xs" ng-click="grid.appScope.removeRecipe(row)"><span class="glyphicon glyphicon-remove"></span></button>' }
             ]
         };
@@ -44,6 +46,31 @@ sharpicApp.controller('recipeController', function($rootScope, $http, $location,
         $scope.recipesOptions.data.splice(index, 1);
     };
 
+    $scope.modifyRecipe = function(entity) {
+        $scope.opts = {
+            dialogFade: false,
+            keyboard: true,
+            templateUrl : 'pages/modifyRecipe.html',
+            controller : modifyRecipeController,
+            resolve: {
+
+            } // empty storage
+        };
+
+        $scope.opts.resolve.recipe = function() {
+            return angular.copy(entity); // {recipeName : entity.recipeName} pass name to Dialog
+        }
+
+        var modalInstance = $modal.open($scope.opts);
+
+        modalInstance.result.then(function(){
+            //on ok button press
+        },function(){
+            //on cancel button press
+            console.log("Modal Closed");
+        });
+    };
+
     $scope.selectClient = function() {
         $http.get('/client/getClientRecipes?clientName=' + $scope.clientName)
             .success(function (data, status, headers, config) {
@@ -51,6 +78,31 @@ sharpicApp.controller('recipeController', function($rootScope, $http, $location,
         })
         .error(function (data, status, header, config) {
         });
+    };
+
+
+})
+
+var modifyRecipeController = function($scope, $modalInstance, $modal, recipe) {
+     $scope.recipe = recipe;
+     $scope.recipeItemOptions = {
+        data: recipe.recipeItems,
+        enableSorting: false,
+        enableColumnMenus: false,
+        columnDefs: [
+            {name: 'clientProduct.name', displayName: 'Product Name', width : '40%' },
+            {name: 'fulls', displayName: 'Fulls', type: 'number' },
+            {name: 'ounces', displayName: 'Ounces', type: 'number' }
+//            {name: 'action', displayName: '', width : '3%', cellTemplate: '<button class="btn btn-danger btn-xs" ng-click="grid.appScope.removeProduct(row)"><span class="glyphicon glyphicon-remove"></span></button>' }
+        ]
+
      };
 
-});
+      $scope.ok = function () {
+        $modalInstance.close();
+      };
+
+      $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+      };
+};
